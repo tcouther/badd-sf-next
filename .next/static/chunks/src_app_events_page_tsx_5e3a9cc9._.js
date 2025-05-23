5d66eb92a38f615a15c11344aefd103fd989e5d3
Terrill Couther <terrill.couther@gmail.com>
2025-05-23 11:14:41 -0700

    first commit

---
+++ .next/static/chunks/src_app_events_page_tsx_5e3a9cc9._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_8271da06._.js",
+  "static/chunks/src_41c319b2._.js",
+  "static/chunks/src_app_page_module_d12eb71c.css"
+],
+    source: "dynamic"
+});
