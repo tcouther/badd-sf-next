673b71700a5c244c7c6f4e45b9a872609a4b5230
Terrill Couther <terrill.couther@gmail.com>
2025-05-25 09:53:10 -0700

    update readme with build and static file ftp deployment instructions

--- .next/static/chunks/src_app_events_page_tsx_5e3a9cc9._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_190852e7._.js",
-  "static/chunks/src_41c319b2._.js",
-  "static/chunks/src_app_page_module_d12eb71c.css"
-],
-    source: "dynamic"
-});
