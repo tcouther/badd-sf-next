31e4925477789b0a3a352155a1b8df37131ea6a8
Terrill Couther <terrill.couther@gmail.com>
2026-01-10 11:31:35 -0800

    restore update events

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   cacheComponents: true,
   /* Uncomment the section below, then "npm run build" a prod static to "/out" folder */
-  /*
   distDir: "out",
   output: "export",
   images: {
@@ -11,7 +10,6 @@ const nextConfig: NextConfig = {
     loaderFile: './ImageLoader.js'
   },
   trailingSlash: true,
-  */
 };
 
 export default nextConfig;
