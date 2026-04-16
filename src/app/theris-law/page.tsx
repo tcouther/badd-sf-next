37eebea09f75d7aa3aa3998b2b86e482f4df1d96
Terrill Couther <terrill.couther@gmail.com>
2026-04-15 20:49:10 -0700

    theris law changes

--- src/app/theris-law/page.tsx
+++ src/app/theris-law/page.tsx
@@ -28,18 +28,25 @@ export default function TherisLawPage() {
             </div>
 
             <div className="general-content">
-              <Link href="/badd-assets/docs/Theris-Law-Fact-Sheet.pdf" className="btn btn-secondary btn-lg mb-4 me-4" target="_blank" aria-label="Print Theris’ Law">
+              <Link href="/badd-assets/docs/Theris-Law-Fact-Sheet.pdf" className="tl-btn btn btn-secondary btn-lg mb-4 me-4" target="_blank" aria-label="Print Theris’ Law">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
                   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1"></path>
                   <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1"></path>
                 </svg> &nbsp; <span>Print Face Sheet</span>
               </Link>
-              <Link href="/badd-assets/docs/Theris-Law-2-2.pdf" className="btn btn-secondary btn-lg mb-4" target="_blank" aria-label="Print Theris’ Law">
+              <Link href="/badd-assets/docs/Theris-Law-2-2.pdf" className="tl-btn btn btn-secondary btn-lg mb-4 me-4" target="_blank" aria-label="Print Theris’ Law">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
                   <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1"></path>
                   <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1"></path>
                 </svg> &nbsp; <span>Print Theris’ Law</span>
               </Link>
+              <Link href="/badd-assets/docs/theris-law-legislative-council-language-rn2604486.pdf" className="tl-btn btn btn-secondary btn-lg mb-4 me-4" target="_blank" aria-label="Print Theris’ Law">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-printer" viewBox="0 0 16 16">
+                  <path d="M2.5 8a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1"></path>
+                  <path d="M5 1a2 2 0 0 0-2 2v2H2a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v1a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-1h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1V3a2 2 0 0 0-2-2zM4 3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2H4zm1 5a2 2 0 0 0-2 2v1H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-1v-1a2 2 0 0 0-2-2zm7 2v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1"></path>
+                </svg> &nbsp; <span>Legislative Council Language</span>
+              </Link>
+              
             </div>
 
             <div className="letter-to-you py-4 text-muted">
